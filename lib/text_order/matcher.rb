a6ce7d47611c0e0ac07b5820618f98ce31850fe1
Jay Hayes <ur@iamvery.com>
2017-10-21 05:41:41 -0500

    Implement failure message interfaces on matcher

--- lib/text_order/matcher.rb
+++ lib/text_order/matcher.rb
@@ -1,13 +1,22 @@
 module TextOrder
   class Matcher
-    attr_reader :expected
+    attr_reader :actual, :expected
 
     def initialize(expected)
       @expected = expected
     end
 
     def matches?(actual)
+      @actual = actual
       actual.include?(expected)
+    end
+
+    def failure_message
+      %{expected "#{actual}" to include the text "#{expected}"}
+    end
+
+    def failure_message_when_negated
+      %{expected "#{actual}" not to include the text "#{expected}"}
     end
   end
 end
--- spec/lib/text_order/matcher_spec.rb
+++ spec/lib/text_order/matcher_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe TextOrder::Matcher do
     expect(matcher.matches?('hahalolwatok')).to be(true)
     expect(matcher.matches?('lolno')).to be(false)
   end
+
+  it 'returns failure message' do
+    matcher = described_class.new('lol')
+    matcher.matches?('wat')
+
+    expect(matcher.failure_message).to eq(%{expected "wat" to include the text "lol"})
+  end
+
+  it 'returns negated failure message' do
+    matcher = described_class.new('lol')
+    matcher.matches?('wat')
+
+    expect(matcher.failure_message_when_negated).to eq(%{expected "wat" not to include the text "lol"})
+  end
 end
